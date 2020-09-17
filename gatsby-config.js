bf7392511b82b95c5d261d070b9a8e78db24ae05
Sumit <sumitsinhassss@outlook.com>
2020-09-17 16:14:07 +0530

    Added gatsby-plugin-google-fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`roboto`, `monserrat\: 400, 400i, 700, 700i`],
+        display: `swap`,
+      },
+    },
   ],
 }

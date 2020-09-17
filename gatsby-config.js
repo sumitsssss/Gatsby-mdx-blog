f4c26a36cffbfe3bd9a294d9c94ee0117b9c3295
Sumit <sumitsinhassss@outlook.com>
2020-09-17 15:58:44 +0530

    Added Gatsby-transformer-sharp and gatsby-plugin-mdx with gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,21 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
